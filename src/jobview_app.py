bc545d52fa9d3bf4fc84f65da27e51ce7daf0597
Derek Weitzel <djw8605@gmail.com>
2013-02-15 10:25:48 -0600

    first try at jsonifying it

--- src/jobview_app.py
+++ src/jobview_app.py
@@ -2,11 +2,14 @@
 import os
 import time
 import ConfigParser
+import re
+import json
 
 from genshi.template import TemplateLoader
 
 from jobs import summarize_jobs
 from cluster_summary import get_cpu_slots
+
 
 initialized = False
 loader = TemplateLoader('templates', auto_reload=True)
@@ -26,22 +29,79 @@ def check_initialized(environ):
         cp = tmp_cp
         initialized = True
 
-def application(environ, start_response):
-    check_initialized(environ)
 
+
+
+def jobs(environ, start_response):
+    global cp
+    status = '200 OK'
+    headers = [('Content-type', 'application/json'),
+              ('Cache-Control', 'max-age=60, public')]
+    start_response(status, headers)
+    
+    jobs, groups, schedd, user = summarize_jobs(cp)
+    global_dict = {'jobs': jobs, 'groups': groups, 'schedd': schedd, 'user': user}
+
+    return [ json.dumps(global_dict) ]
+    
+    
+
+def cluster(environ, start_response):
+    global cp
+    status = '200 OK'
+    headers = [('Content-type', 'application/json'),
+              ('Cache-Control', 'max-age=60, public')]
+    start_response(status, headers)
+    
+    total_slots, free_slots = get_cpu_slots(cp)
+    cpu_slots = {'total': total_slots, 'free': free_slots}
+    
+    return [ json.dumps(cpu_slots) ]
+    
+
+def index(environ, start_response):
     status = '200 OK' # HTTP Status
     headers = [('Content-type', 'text/html'),
               ('Cache-Control', 'max-age=60, public')]
     start_response(status, headers)
-
+    
+    tmpl = loader.load('index.html')
+    
     site_name = cp.get("jobview", "site_name")
 
-    total_slots, free_slots = get_cpu_slots(cp)
-    cpu_slots = {'total': total_slots, 'free': free_slots}
+    return [tmpl.generate(site_name=site_name).render('html', doctype='html')]
+    
 
-    jobs, groups, schedd, user = summarize_jobs(cp)
+def not_found(environ, start_response):
+    pass
 
-    tmpl = loader.load('index.html')
 
-    return [tmpl.generate(site_name=site_name, schedds=schedd, groups=groups, cpu_slots=cpu_slots, jobs=jobs).render('html', doctype='html')]
+urls = [
+    (r'^$', index),
+    (r'jobs/?$', jobs),
+    (r'cluster/?$', cluster)
+]
+
+def application(environ, start_response):
+    check_initialized(environ)
+
+
+    path = environ.get('PATH_INFO', '').lstrip('/')
+    for regex, callback in urls:
+        match = re.search(regex, path)
+        if match is not None:
+            environ['jobview.url_args'] = match.groups()
+            return callback(environ, start_response)
+    return not_found(environ, start_response)
+
+    #site_name = cp.get("jobview", "site_name")
+
+    #total_slots, free_slots = get_cpu_slots(cp)
+    #cpu_slots = {'total': total_slots, 'free': free_slots}
+
+    #jobs, groups, schedd, user = summarize_jobs(cp)
+
+    #tmpl = loader.load('index.html')
+
+    #return [tmpl.generate(site_name=site_name, schedds=schedd, groups=groups, cpu_slots=cpu_slots, jobs=jobs).render('html', doctype='html')]
 
